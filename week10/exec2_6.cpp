5eaac37c5edfd584b461e7783a9621c5218285f3
luoping <luoping0425@gmail.com>
2017-04-11 08:07:17 +0800

    week10 exec2.6

--- week10/exec2_6.cpp
+++ week10/exec2_6.cpp
@@ -4,22 +4,23 @@ using namespace std;
 int main(){
   int n, m;
   int a[20];
-  int b[20];
-  int k;
+  int last;
   cin >> n >> m;
   for(int i = 0; i < n; i++){
     cin >> a[i];
   } 
 
+  int k = 1;
+  for(k = 1; k <= m; k++){
+    last = a[n-1];
+    for(int i = n - 1; i >= 0; i--){
+      a[i] = a[i-1];
+    }
+    a[0] = last;
+  }
   for(int i = 0; i < n; i++){
-    k = (i+m)%n;
-    b[k] = a[i];
-  } 
-
-  for(int i = 0; i < n; i++){
-    cout << b[i] << ' ';
+    cout << a[i] << ' ';
   }
   cout << endl;
-
   return 0;
 }
