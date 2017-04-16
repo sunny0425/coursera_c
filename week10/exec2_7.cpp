5761341014e58f5c90b49f277ccb83b2d6b9f8d9
luoping <luoping0425@gmail.com>
2017-04-16 10:04:59 +0800

    week10 exec2.7

--- week10/exec2_7.cpp
+++ week10/exec2_7.cpp
@@ -3,56 +3,51 @@ using namespace std;
 
 int main(){
   int n;
-  int arr[100];
-  while(cin >> n && n > 0 ){
+  while(cin >> n && n){
+    int arr[100];
+
     for(int i = 0; i < n; i++){
       cin >> arr[i];
     }
     
     if( n == 1){
       cout << arr[0] << endl;
-      continue;
-    }
+    } 
+
     if( n == 2){
       cout << (arr[0] + arr[1]) / 2 << endl;
-      continue;
-    }
-  
-    int left[100], right[100];
-    int avg = 0;
-    for(int i = 0; i < n; i++){
-      int x = 0;
-      int y = 0;
-      for(int k = 0; k < n; k++){
-        if(i == k) continue;
-        if(arr[k] < arr[i]){
-          left[x] = arr[k];
-          x++;
-        } else{
-          right[y] = arr[k];
-          y++;
-        }
-      }
-      if( x != 0) {
-        x = x-1;
-      }
-      if (y != 0){
-        y = y-1;
-      }
-      if(n % 2 == 1){
-        if( x == y){
-          cout << arr[i] << endl;
-          break;
-        }  
-      } else {
-        if( x == y + 1 || x == y - 1){
-          avg += arr[i];
-        }
-      }
     }
 
-    if(n % 2 == 0){
-      cout << avg/2 << endl;
+    if( n > 2) {
+      int sum = 0;
+      for(int i = 0; i < n; i++){
+        int small = 0;
+        int big = 0;
+        for(int k = 0; k < n; k++){
+          if(i == k) continue;
+          if(arr[k] < arr[i]){
+            small += 1;
+          } else {
+            big += 1;
+          }
+        }
+        if( n % 2 != 0){
+          // 奇数
+          // cout << i << ' ' << small << ' ' << big << endl;
+          if(small == big){
+            cout << arr[i] << endl;
+            break;
+          }
+        } else {
+          // 偶数
+          if(small == big + 1 || small == big - 1){
+            sum += arr[i];
+          }
+        }
+      }
+      if( n % 2 == 0){
+        cout << sum / 2 << endl;
+      }
     }
   }
   
