74b8d5cb739ebc355ef3da532e889b6e18465951
luoping <luoping0425@gmail.com>
2017-04-20 08:03:02 +0800

    week12

---
+++ week12/exec1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int year;
+  cin >> year)
+  if( year % 4 == 0){
+    if(year % 100 == 0 && (year % 3200 == 0 || year % 400 != 0)){
+      cout << 'N' << endl;
+    } 
+    else {
+      cout << 'Y' << endl;
+    }
+  } else {
+    cout << 'N' << endl;
+  }
+  return 0;
+}
