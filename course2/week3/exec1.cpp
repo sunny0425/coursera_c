4ed0662e258a8134f94de8db5e1b085b1b183611
luoping <luoping0425@gmail.com>
2017-05-18 07:02:30 +0800

    week3 exec1

--- course2/week3/exec1.cpp
+++ course2/week3/exec1.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int overturn(char word[50]){
-  for(int i = 0; word[i] != '\0'; i++){
-    cout << word[i];
+int overturn(char str[501], int x, int y){
+  if( x < y){
+    overturn(str, x+1, y);
+    cout << str[x];
+  } else if( x == y){
+    cout << str[x];
   }
-  // cout << ' ';
+
   return 0;
 }
 
 int main(){
-  char str[500];
-  char word[50];
+  char str[501];
   cin.getline(str, 500);
-  int j = 0;
-  for(int i = 0; str[i] != '\0'; i++){
-    cout << str[i];
-    if(str[i] != ' '){
-      cout << j;
-      word[j] = str[i];
-      j++;
-      // cout << word[j];
-    } else {
-      word[j+1] = '\0';
-      overturn(word);
 
-      j = 0;
+  int len = strlen(str);
+
+  int x = 0;
+  int y;
+
+  for(int i = 0; i < len+1; i++){
+    if(str[i] == ' ' || str[i] == '\0' || str[i] = '\t'){
+      y = i;
+      if( x != y){
+        overturn(str, x, y-1);
+      }
+      cout << str[i];
+      x = i + 1;
     }
   }
 
